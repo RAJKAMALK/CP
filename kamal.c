1a973ef698299122621359640d50b28edb2f935b
RAJKAMALK <kamalkrish95@gmail.com>
2016-06-28 11:19:11 +0000

    Add files via upload

---
+++ kamal.c
@@ -0,0 +1,20 @@
+#include<string.h>
+int main()
+{
+char str[100];
+int i,j=0,temp;
+printf("\nenter the string");
+gets(str);
+i=0;
+j=strlen(str)-1;
+while(i,j)
+{
+temp=str[i];
+str[i]=str[j];
+str[j]=temp;
+i++;
+j--;
+}
+printf("\nreverse of string is %s",str);
+return0;
+}
